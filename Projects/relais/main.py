d3b6207d61e32c5bbbbb89f5b5b3888ae25c1f40
Anton Vanhoucke <antonvh@gmail.com>
2021-03-18 18:03:51 +0100

    Added EV3 relais switching code

---
+++ Projects/relais/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.iodevices import AnalogSensor
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+
+# Write your program here.
+ev3.speaker.beep()
+
+relais = AnalogSensor(Port.S1)
+
+while True:
+    relais.active()
+    wait(1000)
+    relais.passive()
+    wait(1000)
