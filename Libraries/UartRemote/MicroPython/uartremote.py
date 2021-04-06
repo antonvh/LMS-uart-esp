3e015dea87149ca69e76711b1ef9ac028b579388
Ste7anSte7an <johndoe@Johns-MacBook-Pro.local>
2021-04-07 00:06:00 +0200

    UartRemote adopted for Spike Prime hub

--- Libraries/UartRemote/MicroPython/uartremote.py
+++ Libraries/UartRemote/MicroPython/uartremote.py
@@ -1,11 +1,14 @@
+
 # this library support the following micropython based platforms
 # - pybricks on EV3
 # - micropython on ESP8266
 # - micropython on ESP32
 # - micropython on OpenMV H7 plus
+# - SPIKE hub
 
-
-platforms={'linux':'EV3','esp32':'ESP32','esp8266':'ESP8266','OpenMV4P-H7':'H7'}
+platforms={'linux':'EV3','esp32':'ESP32','esp8266':'ESP8266',\
+            'OpenMV4P-H7':'H7','LEGO Learning System Hub':'SPIKE'
+            }
 
 import sys
 platform='unknown'
@@ -29,33 +32,20 @@ elif PLATFORM=="EV3":
     from pybricks.parameters import Port
 elif PLATFORM=="H7":
     from pyb import UART
-
-
-"""
-Protocol used by this library:
-
-'<'+<l><lc><cmd><lf><f><data>+'>'
-
-<l> = total length of packet
-<lc> = length command string
-<cmd> = command (3 characters)
-<lf> = length of format string
-<f> = format string, as in struct, e.g. 'b'=byte, 'i'=integer,  etc
-<data> = encoded data
-"""
-
-
+elif PLATFORM=="SPIKE":
+    from hub import port
+    from utime import sleep_ms
 
 class UartRemote:
-    commands={}  
+    commands={}
 
     def digitformat(self,f):
-       nn='0'
-       i=0
-       while f[i]>='0' and f[i]<='9':
+        nn='0'
+        i=0
+        while f[i]>='0' and f[i]<='9':
                 nn+=f[i]
                 i+=1
-       return (int(nn),f[i:])     
+        return (int(nn),f[i:])
 
 
     def __init__(self,*port_lego,baudrate=230400,timeout=1000,debug=False):
@@ -63,13 +53,17 @@ class UartRemote:
             port=port_lego[0]
             self.uart = UARTDevice(port,baudrate=baudrate,timeout=timeout)
         elif PLATFORM=="H7":
-            self.uart = UART(3, baudrate, timeout_char=timeout)                         # P4,P5 default self.uart
-        elif PLATFORM=="ESP8266":    
+            self.uart = UART(3, baudrate, timeout_char=timeout)
+        elif PLATFORM=="ESP8266":
             self.uart = UART(0,baudrate=baudrate,timeout=timeout,rxbuf=100)
         elif PLATFORM=="ESP32":
-            # default pins for uart 1 collide with SPI for PS-RAM or flash
-            # therefore, use other pins, e.g. GPIO18 and GPIO19    
             self.uart = UART(1,rx=18,tx=19,baudrate=baudrate,timeout=timeout)
+        elif PLATFORM=="SPIKE":
+            port=port_lego[0]
+            self.uart = port
+            self.uart.mode(1)
+            sleep_ms(1000)# wait for all duplex methods to appear
+            self.uart.baud(baudrate) # set baud rate
         else:
             raise RuntimeError('MicroPython Platform not defined')
         self.DEBUG=debug
@@ -91,7 +85,7 @@ class UartRemote:
                     fo=f[0]
                     data=argv[1+i]
                     td=type(data)
-                    if td==list: 
+                    if td==list:
                         n=len(data)
                         ff+="a%d"%n+fo
                         for d in data:
@@ -103,7 +97,7 @@ class UartRemote:
                     else:
                         ff+=fo
                         s+=struct.pack(fo,data)
-                
+
                 else:
                     fo="%d"%nf+f[0]
                     data=argv[1+i:1+i+nf]
@@ -111,12 +105,12 @@ class UartRemote:
                     s+=struct.pack(fo,*data)
                 i+=nf
                 f=f[1:]
-            s=struct.pack('B',len(ff))+ff.encode('utf-8')+s 
+            s=struct.pack('B',len(ff))+ff.encode('utf-8')+s
         else:
-            s=b'\x01z'  # dummy format 'z' for no arguments
+            s=b'\x01z'# dummy format 'z' for no arguments
         s=struct.pack("B",len(cmd))+cmd.encode('utf-8')+s
-        s=struct.pack("B",len(s))+s 
-        return s 
+        s=struct.pack("B",len(s))+s
+        return s
 
     def decode(self,s):
         sizes={'b':1,'B':1,'i':4,'I':4,'f':4,'s':1}
@@ -131,7 +125,7 @@ class UartRemote:
         f=s[p:p+nf].decode('utf-8')
         p+=nf
         data=()
-        if f=="z":  # dummy format 'z' for empty data
+        if f=="z":# dummy format 'z' for empty data
             return cmd,None
         while (len(f)>0):
             nf,f=self.digitformat(f)
@@ -142,7 +136,7 @@ class UartRemote:
                 fo=f[0]
                 nr_bytes=nf*sizes[fo]
                 data=data+(list(struct.unpack("%d"%nf+fo,s[p:p+nr_bytes])),)
-            else:                           
+            else:
                 ff=fo if nf==0 else "%d"%nf+fo
                 if nf==0: nf=1
                 nr_bytes=nf*sizes[fo]
@@ -155,8 +149,6 @@ class UartRemote:
             return "error","len"
         else:
             return cmd,data
-
-
     def available(self):
         if PLATFORM=="EV3":
             return self.uart.waiting()
@@ -168,20 +160,31 @@ class UartRemote:
         if PLATFORM=="EV3":
             if self.uart.waiting()>0:
                 self.uart.read_all()
+        elif PLATFORM=="SPIKE":
+            r=b'1'
+            while r!=b'':
+                r=self.uart.read(1)
         else:
             if self.uart.any()!=0:
                 self.uart.read()
-        
+
     def receive(self):
+        delim=b""
         if PLATFORM=="EV3":
             while (self.uart.waiting()==0):
                 pass
+        elif PLATFORM=="SPIKE":
+            c=b''
+            while c==b'':
+                c=self.uart.read(1)
+            delim=c
         else:
             while (self.uart.any()==0):
                 time.sleep(0.01)
                 pass
         try:
-            delim=self.uart.read(1)
+            if delim==b'':
+                delim=self.uart.read(1)
             if delim!=b'<':
                 self.flush()
                 return ("err","nok")
@@ -200,7 +203,7 @@ class UartRemote:
                 return ("err","nok")
             else:
                 result=self.decode(s)
-            
+
         except:
             self.flush()
             result=("err","nok")
@@ -229,7 +232,7 @@ class UartRemote:
 
     def wait_for_command(self):
         command,value=self.receive()
-        if command[-3:]!='ack':   # discard any ack from other command
+        if command[-3:]!='ack':# discard any ack from other command
             if command in self.commands:
                 command_ack=command+"ack"
                 if value!=None:
@@ -254,4 +257,3 @@ class UartRemote:
                 self.wait_for_command()
             except:
                 self.flush()
-
