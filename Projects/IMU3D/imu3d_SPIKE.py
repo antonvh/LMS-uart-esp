7b46e65d4b5ed55908289286bdbe7bc7e2c9a675
ste7anste7an <jeroen.opdebeek@gmail.com>
2021-05-02 16:04:59 +0200

    imu3d vpython example added

---
+++ Projects/IMU3D/imu3d_SPIKE.py
@@ -0,0 +1,124 @@
+import hub
+from utime import sleep_ms,ticks_ms,ticks_diff,sleep
+from projects.uartremote import *
+
+MAINPY="""from ws_connection import ClientClosedError
+from ws_server import WebSocketServer, WebSocketClient
+import time
+
+from uartremote import *
+
+class TestClient(WebSocketClient):
+
+    def __init__(self, conn):
+        super().__init__(conn)
+
+    def process(self):
+        try:
+            msg = self.connection.read()
+
+            if u.available():
+                (cmd,value)=u.receive_command()
+                u.send_command(cmd+"ack",'s','ok')
+                print(value)
+                s=cmd+" "+",".join(["%f"%i for i in value])
+                self.connection.write(s)
+            
+            if not msg:
+                return
+            msg = msg.decode("utf-8")
+            items = msg.split(" ")
+            cmd = items[0]
+            if cmd == "wsopen":
+                #self.connection.write(cmd + " World")
+                print("ws connection opened")
+                #u.disable_repl_locally()
+                #u.flush()
+                u.send_command("wsopen",'s','ok')
+            elif cmd=="euler":
+                u.send_command("euler",'s','ok')
+        except ClientClosedError:
+            self.connection.close()
+
+
+class TestServer(WebSocketServer):
+    def __init__(self):
+        super().__init__("graphlog.html", 2)
+
+    def _make_client(self, conn):
+        return TestClient(conn)
+
+u=UartRemote()
+u.disable_repl_locally()
+server = TestServer()
+server.start()
+try:
+    while True:
+        server.process_all()
+except KeyboardInterrupt:
+    pass
+server.stop()
+"""
+
+WIFI="""import network
+for i in (network.AP_IF, network.STA_IF):
+    iface = network.WLAN(i)
+    if iface.active():
+        print("WebSocket started on ws://%s" % iface.ifconfig()[0])
+"""
+
+
+ur=UartRemote("A") # connect ESP to port A
+
+
+ur.flush() # remove evernything
+# try to enable repl if esp is still in non_repl mode
+ur.send_command("enable repl",'s','ok')
+print("response enable repl",ur.uart.read(100))
+
+
+ur.repl_activate()
+print(ur.repl_run("print('Repl Tested')"))
+print(ur.repl_run(WIFI))
+print(ur.repl_run(MAINPY,reply=False))
+print("script is running")
+ur.flush()
+ws_opened=False
+sleep_ms(1000)
+
+refresh_ms=100# refresh every 100ms
+t_old=ticks_ms()
+
+ACC='acc'
+GYRO='gyro'
+EULER='euler'
+
+cur_mode=EULER
+
+while True:
+    if cur_mode==ACC:
+        value=list(hub.motion.accelerometer())
+    elif cur_mode==EULER:
+        value=list(hub.motion.yaw_pitch_roll())
+    if ws_opened:
+        t_ms=ticks_ms()
+        if ticks_diff(t_ms,t_old)>refresh_ms:
+            ur.call(cur_mode,'f',value)
+            t_old=t_ms
+    if ur.available():
+        cmd,val=ur.receive_command(wait=False)
+        print("received:",cmd,val)
+        if cmd=="wsopen": # if websocket is opened, initialize chart
+            print("WebSocket opened")
+            sleep_ms(500) # wait until chart is initialized
+            ws_opened=True
+        elif cmd=="acc":
+            cur_mode=ACC
+        elif cmd=="euler":
+            print("cmd=",cmd)
+            cur_mode="euler"
+
+
+ur.flush()
+print("Flushed")
+
---
+++ Projects/IMU3D/imu3d_vpython.py
@@ -0,0 +1,98 @@
+# install vpython, see https://vpython.org/presentation2018/install.html
+
+
+
+import math
+import numpy as np
+from vpython import *
+
+import time
+import sys,os
+import websocket
+
+# websocket initialization
+
+websocket.enableTrace(False)
+ws = websocket.WebSocket()
+ws.connect("ws://192.168.x.x:80")  # replace with IP address of ESP module
+ws.send("wsopen")
+ws.send("euler")
+ACC='acc'
+EULER='euler'
+
+# conversion
+
+toRad = np.pi/180
+toDeg = 1/toRad
+
+
+# set the scene
+scene.range=15
+scene.forward=vector(-1,-1,-1)
+scene.width=600
+scene.height=600
+
+
+
+# define fixed axes frame
+xArrow=arrow(length=8,shaftwidth=.3, color=color.red, axis=vector(1,0,0))
+yArrow=arrow(length=8,shaftwidth=.3, color=color.green, axis=vector(0,1,0))
+zArrow=arrow(length=8,shaftwidth=.3, color=color.blue, axis=vector(0,0,1))
+
+# define frame with axes moving with the object
+frontArrow=arrow(length=4,shaftwidth=.3, color=color.purple, axis=vector(1,0,0))
+upArrow=arrow(length=1,shaftwidth=.3, color=color.magenta, axis=vector(0,1,0))
+sideArrow=arrow(length=2,shaftwidth=.3, color=color.orange, axis=vector(0,0,1))
+
+# create a 'SPIKE'
+bSpike=box(length=12,width=7,height=1,opacity=0.8,pos=vector(0,1,0))
+bSpikebottom=box(length=12,width=7,height=3,opacity=0.8,color=color.yellow,pos=vector(0,-1,0))
+bSpiktbtnL=box(length=1,width=2,height=0.01,pos=vector(5,1.5,2),color=color.white)
+bSpiktbtnR=box(length=1,width=2,height=0.01,pos=vector(5,1.5,-2),color=color.white)
+cSpikebtn=cylinder(radius=1,pos=vector(5,1.5,0),axis=vector(0,.01,0),color=color.white)
+cSpikeblue=cylinder(radius=.5,pos=vector(-4.5,1.5,-2),opacity=0.5,axis=vector(0,.01,0),color=color.blue)
+
+# set all in a single compounf object
+Spike=compound([bSpike,bSpikebottom,bSpiktbtnL,bSpiktbtnR,cSpikebtn,cSpikeblue])
+
+while True:
+    try:
+        recv=None
+        while recv==None:
+            recv= ws.recv()
+            if recv!=None:
+                s=recv.split(' ')
+                cmd,value=s[0],s[1:][0]  # split result in command and value
+                ret=[float(ii) for ii in value.split(',')]
+                #if cmd==EULER:
+                #    ret=fromeuler(*tuple(ret))
+                #print(recv,ret)
+        y,p,r=ret # order of values is yaw, roll, pitch
+        # convert to radians
+        yaw=y*toRad
+        pitch=-p*toRad # change sign
+        roll=-r*toRad # change sign
+        # up vector as function of yaw and pitch
+        k=vector(cos(yaw)*cos(pitch),sin(pitch),sin(yaw)*cos(pitch))
+        
+        y=vector(0,1,0) # helper vector for calculating cross product
+        s=cross(k,y) # perpendicular to y and k
+        v=cross(s,k) # perpendicular ro s and k
+        vrot=v*cos(roll)+cross(k,v)*sin(roll) # rodriguez rotation (k.v=0)
+
+        frontArrow.axis=k
+        upArrow.axis=vrot
+        sideArrow.axis=cross(k,vrot)
+
+        upArrow.length=6
+        frontArrow.length=10
+        sideArrow.length=6
+
+        Spike.axis=k
+        Spike.up=vrot
+    except KeyboardInterrupt:
+        ws.close()
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
