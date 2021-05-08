f9efea352c807a4b320c6c4ef058354b1c4276d9
Anton Vanhoucke <antonvh@gmail.com>
2021-05-08 10:36:00 +0200

    Added refresh rates for reference

--- Projects/NeoPixel SPIKE hsl/spike_neopixel_repl_load_script.py
+++ Projects/NeoPixel SPIKE hsl/spike_neopixel_repl_load_script.py
@@ -1,11 +1,27 @@
 # Copy this script in a new Python project in the SPIKE Prima app to run it
-# Upload uartremote.mpy to the esp breakout board using the webrepl.
+
+# First, Install uartremote on the SPIKE with the installer script here:
+# /Libraries/UartRemote/MicroPython/SPIKE/install_uartremote_mpy.py
+
+# On the ESP/breakout Upload uartremote.mpy using the webrepl (see wiki).
 # No need for a main.py. It is executed via the raw repl over serial.
-# Install uartremote on the SPIKE with the installer script.
+
+# Note that this script increases the clock frequency of the esp to 160MHz!
+
+# Typical output of this program:
+# [10:29:45.294] > loaded script
+# [10:29:47.823] > Paint with raw repl pixel set succeeded at 40.3714 updates per second.
+# [10:29:47.847] > Entered remote command processing loop
+# [10:29:47.915] > ('init_pixelsack', 'ok')
+# [10:30:08.614] > Paint with 'B' color array succeeded at 4.83652 updates per second.
+# [10:30:19.161] > Pixel setting & paint succeeded at 9.5347 updates per second.
+# [10:30:24.112] > Direct buffer paint with 'r' succeeded at 20.3046 updates per second.
+# [10:30:27.053] > Direct buffer paint with 'raw' succeeded at 34.2701 updates per second.
 
 MAINPY="""
 from uartremote import *
 from neopixel import NeoPixel
+machine.freq(160000000)
 
 ur = UartRemote()
 color = (255,0,255)
@@ -27,7 +43,7 @@ def paint():
 
 def paint_buffer(bgr_bytes):
     global np
-    np.buf=bytearray(bgr_bytes)
+    np.buf=bgr_bytes
     np.write()
 
 def paint_colors(*colors):
@@ -82,18 +98,6 @@ for n in range(N_UPDATES):
     ur.call('paint_colors', N_LEDS*'B',*colors)
 duration = utime.ticks_diff(utime.ticks_ms(), start)
 print("Paint with 'B' color array succeeded at {} updates per second.".format(N_UPDATES/duration*1000))
-
-start = utime.ticks_ms()
-for n in range(N_UPDATES):
-    i = n % N_LEDS
-    buf = i*b'\x00\x00\x00' + bytes((255,0,255)) + (N_LEDS-i-1)*b'\x00\x00\x00'
-    ur.repl_run(
-"""
-np.buf=bytearray({})
-np.write()""".format(buf)
-            )
-duration = utime.ticks_diff(utime.ticks_ms(), start)
-print("Direct buffer paint through repl succeeded at {} updates per second.".format(N_UPDATES/duration*1000))
 
 start = utime.ticks_ms()
 for n in range(N_UPDATES):
