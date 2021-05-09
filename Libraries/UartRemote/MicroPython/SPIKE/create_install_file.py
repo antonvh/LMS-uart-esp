a78c93f6175a6366277ad6ccacbaba66fefe9bbb
Anton Vanhoucke <antonvh@gmail.com>
2021-05-09 13:15:57 +0200

    Install script now removes old py library files

--- Libraries/UartRemote/MicroPython/SPIKE/create_install_file.py
+++ Libraries/UartRemote/MicroPython/SPIKE/create_install_file.py
@@ -14,10 +14,21 @@ time.sleep(1)
 uartremote=open('uartremote.mpy','rb').read()
 ur_b64=binascii.b2a_base64(uartremote)
 
-spike_code="import ubinascii\nb64=\"\"\""+ur_b64.decode('utf-8')+"\"\"\"\n\n"
-spike_code+="uartremote=ubinascii.a2b_base64(b64)\n\n"
-spike_code+="print('writing uartremote.mpy to folder /projects')\n"
-spike_code+="open('/projects/uartremote.mpy','wb').write(uartremote)\n"
-spike_code+="print('Finished writing uartremote.mpy')\n"
+spike_code="import ubinascii, uos, machine\nb64=\"\"\""+ur_b64.decode('utf-8')+"\"\"\"\n\n"
+spike_code+="""
+uartremote=ubinascii.a2b_base64(b64)
+
+try:
+    uos.remove('/projects/uartremote.py')
+    uos.remove('/projects/uartremote.mpy')
+except OSError:
+    pass
+
+print('writing uartremote.mpy to folder /projects')
+print('writing uartremote.mpy to folder /projects')
+open('/projects/uartremote.mpy','wb').write(uartremote)
+print('Finished writing uartremote.mpy')
+machine.reset()
+"""
 
 open('install_uartremote.py','w').write(spike_code)
