df1ec5d8237d36dfa466b768471d7913857660e1
Anton Vanhoucke <antonvh@gmail.com>
2021-05-13 10:10:25 +0200

    added SPIKE test script

---
+++ Libraries/UartRemote/MicroPython/SPIKE/test_esp_uartremote_comms.py
@@ -0,0 +1,30 @@
+from projects.uartremote import *
+from utime import ticks_ms
+ur = UartRemote('D')
+
+print("Uart initialized")
+print(ur.version)
+
+# Test REPL
+ur.repl_activate()
+print(ur.repl_run("print('Repl tested')"))
+print(ur.repl_run("from uartremote import *"))
+print(ur.repl_run("ur=UartRemote()"))
+
+# Start command loop
+print(ur.repl_run("ur.loop()",reply=False))
+ur.flush()
+print(ur.call('echo','repr','Uart command loop tested with echo'))
+
+# Test if we can revert to REPL
+print(ur.call('enable repl'))
+ur.repl_activate()
+print(ur.repl_run("print('Repl tested')"))
+print(ur.repl_run("ur.loop()",reply=False))
+
+# Test the speed of the command loop
+start = ticks_ms()
+for i in range(100):
+    ur.call('echo','bb',1,2)
+elapsed=ticks_ms()-start
+print("Commands per second: ",100*1000/elapsed)
