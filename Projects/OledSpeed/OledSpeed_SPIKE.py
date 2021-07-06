cc8cf12907b585fef953a989c845c5be761d7189
Jeroen Op de Beke <jeroen.opdebeek@gmail.com>
2021-07-06 14:55:08 +0200

    extra reset of esp8266 module

--- Projects/OledSpeed/OledSpeed_SPIKE.py
+++ Projects/OledSpeed/OledSpeed_SPIKE.py
@@ -1,4 +1,5 @@
 from spike import Motor
+import time
 from projects.uartremote import *
 
 MAINPY="""
@@ -11,9 +12,17 @@ ur=UartRemote("E") # connect ESP to port A
 ur.flush() # remove evernything
 ur.repl_activate()
 print(ur.repl_run("print('Repl Tested')"))
-print(ur.repl_run(MAINPY,reply=False))
-print(ur.uart.read(1024))
+# reset esp8266; otherwise oled_speed library is not import correctly after first time
+ur.repl_run("import machine")
+ur.repl_run("machine.reset()")
+time.sleep_ms(500)
+ur.flush() # remove evernything
+ur.repl_activate()
+ur.repl_run(MAINPY,reply=False)
 print("loaded script")
+
+
+
 
 
 
