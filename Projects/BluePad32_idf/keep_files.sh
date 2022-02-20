19851d8efe2bf2b98b320b873918a45d465008b7
ste7anste7an <ste7an@gmail.com>
2022-02-20 07:39:59 -0800

    try to fix files_Bluepad directories

--- Projects/BluePad32_idf/keep_files.sh
+++
@@ -1,5 +0,0 @@
-export PROJECT_DIR=$1
-export FILES_DIR=files_$PROJECT_DIR
-echo $FILES_DIR
-cd $PROJECT_DIR
-cat ../$FILES_DIR/files_to_copy.txt | while read f; do cp --parents -r $f ../$FILES_DIR; done
--- Projects/BluePad32_idf/restore_files.sh
+++
@@ -1,5 +0,0 @@
-export PROJECT_DIR=$1
-export FILES_DIR=files_$PROJECT_DIR
-echo $FILES_DIR
-cd $FILES_DIR
-cat files_to_copy.txt | while read f; do cp --parents -r $f ../$PROJECT_DIR; done
