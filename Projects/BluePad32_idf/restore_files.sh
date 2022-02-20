d8e571c4abe2902d23540942776366188da7eab9
ste7anste7an <ste7an@gmail.com>
2022-02-20 02:16:22 -0800

    small error in script

--- Projects/BluePad32_idf/restore_files.sh
+++ Projects/BluePad32_idf/restore_files.sh
@@ -1,3 +1,5 @@
-export WORKING_DIR=BluePad32_Uartremote
-export PROJECT_DIR=files_Bluepad32_Uartremote
+export PROJECT_DIR=$1
+export FILES_DIR=files_$PROJECT_DIR
+echo $FILES_DIR
+cd $FILES_DIR
 cat files_to_copy.txt | while read f; do cp --parents -r $f ../$PROJECT_DIR; done
